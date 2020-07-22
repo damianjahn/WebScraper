7ab0086f0154920c9903c4db48d2ba1b1a083f71
Damian <damian.jahn@icloud.com>
2020-07-22 15:42:14 +0200

    Some impovments

--- images.py
+++ images.py
@@ -2,16 +2,32 @@ from bs4 import BeautifulSoup
 import requests
 from PIL import Image
 from io import BytesIO
+import os
 
-search = input("Search for:")
-params = {"q": search}
-r = requests.get("https://www.bing.com/images/search", params=params)
+def StartSearch():
+    search = input("Search for:")
+    params = {"q": search}
+    dir_name = search.replace(" ", "_").lower()
 
-soup = BeautifulSoup(r.text, "html.parser")
-links = soup.findAll("a", {"class": "thumb"})
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
 
-for item in links:
-    img_obj = requests.get(item.attrs["href"])
-    title = item.attrs["href"].split("/")(-1)
-    img = Image.open(BytesIO(img_obj.content))
-    img.save("./scraped_images" + title, img.format)
+    r = requests.get("https://www.bing.com/images/search", params=params)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = soup.findAll("a", {"class": "thumb"})
+
+    for item in links:
+        img_obj = requests.get(item.attrs["href"])
+        title = item.attrs["href"].split("/")(-1)
+        try:
+            img = Image.open(BytesIO(img_obj.content))
+            img.save("./" + dir_name + "/" + title, img.format)
+        except:
+            print("Could not save an image")
+
+    StartSearch()
+
+StartSearch()
+
+
